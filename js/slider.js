7d97fadf4f6015f2cc55e6fbbff5a782546b94f5
Manoj Kumar <66679734+manojkumar44@users.noreply.github.com>
2023-06-20 08:52:15 +0100

    Update slider.js
    Following changes made:
    
    - The left and right arrow keys did not work nor did the the btn_prev and btn_next buttons have any effect when the slider first loaded on the page. This was due to the event listeners being set up before the elements were being fully loaded and made accessible in the DOM. This issue was resolved by wrapping the code in an event listener that triggers when the DOM content loaded.
    
    - In this modified code, event listeners are added for mouse events (mousedown, mouseup, and mousemove), touch events (touchstart, touchend, and touchmove), and keyboard events (keydown). Functions are provided to handle each event type, and the handleDrag function is responsible for updating the slider position based on the drag or touch movement.
    
    - When we are on the first slide (translateX(0px)) and we press the 'left arrow' key, we need to replicate the behavior of when we press the 'prev_btn' , meaning, it should slide all the way to the last slide.  The same behavior should occur if we are on the last slide and the user presses the 'next_btn',  it should slide all the way back to the first slide.

--- js/slider.js
+++ js/slider.js
@@ -1,34 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Get the id of the previous button and store it in a variable named prevBtn
   var prevBtn = document.getElementById('btn_prev');
-
-  // Get the id of the next button and store it in a variable named nextBtn
   var nextBtn = document.getElementById('btn_next');
-
-  // Get ALL the 'li' elements which house the slide images
-  var slides = Array.from(document.getElementsByTagName('li')); // Convert HTMLCollection to an Array for easier manipulation
-
-  // Get the actual slider ul which contains all the li's
+  var slides = Array.from(document.getElementsByTagName('li'));
   var slider = document.getElementById('slider_ul');
-
-  // Get the display panel
   var displayTrans = document.getElementById('scrollDisplayTrans');
-  displayTrans.style.color = "#ffffff";
-
   var displayStartPos = document.getElementById('scrollDisplayPos');
-  displayStartPos.style.color = "#ffffff";
 
-  // Obtain the total no. of slides
   var totalSlides = slides.length;
-
-  // Create variables for previous and next slides as well as the first and last slides.
-  var firstSlide = slides[0];
-  var lastSlide = slides[totalSlides - 1];
-
-  // Calculate the width of each li element (one slide)
-  var slideWidth = firstSlide.offsetWidth;
-
-  // Calculate the combined width of all the slides
+  var slideWidth = slides[0].offsetWidth;
   var totalSlidesWidth = slideWidth * (totalSlides - 1);
 
   var startPos = 0;
@@ -36,7 +15,6 @@ document.addEventListener("DOMContentLoaded", function() {
   displayTrans.innerText = slider.style.transform = "translateX(0px)";
   displayStartPos.innerText = startPos + "px";
 
-  // Add event listeners for mouse and touch events
   slider.addEventListener('mousedown', handleMouseDown);
   slider.addEventListener('mouseup', handleMouseUp);
   slider.addEventListener('mousemove', handleMouseMove);
@@ -44,26 +22,22 @@ document.addEventListener("DOMContentLoaded", function() {
   slider.addEventListener('touchend', handleTouchEnd);
   slider.addEventListener('touchmove', handleTouchMove);
 
-  // Add event listener for keyboard arrow keys
   document.addEventListener('keydown', handleKeyDown);
 
-  // Function to handle mouse down event
   function handleMouseDown(event) {
     event.preventDefault();
     startPos = getTranslateXValue();
     dragStartX = event.clientX;
-    slider.classList.add('slider-transition'); // Add CSS transition for smooth animation
+    slider.classList.add('slider-transition');
   }
 
-  // Function to handle mouse up event
   function handleMouseUp(event) {
     event.preventDefault();
     var dragEndX = event.clientX;
     handleDrag(dragEndX);
-    slider.classList.remove('slider-transition'); // Remove CSS transition to stop animation
+    slider.classList.remove('slider-transition');
   }
 
-  // Function to handle mouse move event
   function handleMouseMove(event) {
     event.preventDefault();
     if (event.buttons === 1) {
@@ -72,40 +46,34 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  // Function to handle touch start event
   function handleTouchStart(event) {
     event.preventDefault();
     startPos = getTranslateXValue();
     touchStartX = event.touches[0].clientX;
-    slider.classList.add('slider-transition'); // Add CSS transition for smooth animation
+    slider.classList.add('slider-transition');
   }
 
-  // Function to handle touch end event
   function handleTouchEnd(event) {
     event.preventDefault();
     var touchEndX = event.changedTouches[0].clientX;
     handleDrag(touchEndX);
-    slider.classList.remove('slider-transition'); // Remove CSS transition to stop animation
+    slider.classList.remove('slider-transition');
   }
 
-  // Function to handle touch move event
   function handleTouchMove(event) {
     event.preventDefault();
     var touchEndX = event.touches[0].clientX;
     handleDrag(touchEndX);
   }
 
-  // Function to handle dragging based on the start and end positions
   function handleDrag(endX) {
     var dragDistance = endX - touchStartX;
     var dragSlides = Math.round(dragDistance / slideWidth);
     var newPos = startPos - (dragSlides * slideWidth);
 
     if (newPos > 0) {
-      // Dragged towards the right
       newPos = 0;
     } else if (newPos < -totalSlidesWidth) {
-      // Dragged towards the left
       newPos = -totalSlidesWidth;
     }
 
@@ -114,39 +82,33 @@ document.addEventListener("DOMContentLoaded", function() {
     displayStartPos.innerText = newPos + "px";
   }
 
-  // Function to handle keyboard arrow keys
   function handleKeyDown(event) {
     switch (event.keyCode) {
-      case 37: // left arrow key
+      case 37:
         if (startPos === 0) {
-          slider.style.transform = "translateX(-" + totalSlidesWidth + "px)";
-          displayTrans.innerText = slider.style.transform;
-          displayStartPos.innerText = "-" + totalSlidesWidth + "px";
+          startPos = -totalSlidesWidth;
         } else {
           startPos += changePos;
-          slider.style.transform = "translateX(" + startPos + "px)";
-          displayTrans.innerText = slider.style.transform;
-          displayStartPos.innerText = startPos + "px";
         }
+        slider.style.transform = "translateX(" + startPos + "px)";
+        displayTrans.innerText = slider.style.transform;
+        displayStartPos.innerText = startPos + "px";
         break;
-      case 39: // right arrow key
+      case 39:
         if (startPos === -totalSlidesWidth) {
-          slider.style.transform = "translateX(0px)";
-          displayTrans.innerText = slider.style.transform;
-          displayStartPos.innerText = "0px";
+          startPos = 0;
         } else {
           startPos -= changePos;
-          slider.style.transform = "translateX(" + startPos + "px)";
-          displayTrans.innerText = slider.style.transform;
-          displayStartPos.innerText = startPos + "px";
         }
+        slider.style.transform = "translateX(" + startPos + "px)";
+        displayTrans.innerText = slider.style.transform;
+        displayStartPos.innerText = startPos + "px";
         break;
       default:
         break;
     }
   }
 
-  // Utility function to get the current translateX value
   function getTranslateXValue() {
     var transform = window.getComputedStyle(slider).getPropertyValue('transform');
     var matrix = new WebKitCSSMatrix(transform);
