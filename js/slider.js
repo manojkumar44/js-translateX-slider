dfe7e96aeb0ddc8d7aab6e3abb949998362e1c8b
Manoj Kumar <66679734+manojkumar44@users.noreply.github.com>
2023-06-20 05:46:03 +0100

    Update slider.js
    In this modified code, event listeners are added for mouse events (mousedown, mouseup, and mousemove), touch events (touchstart, touchend, and touchmove), and keyboard events (keydown). Functions are provided to handle each event type, and the handleDrag function is responsible for updating the slider position based on the drag or touch movement.

--- js/slider.js
+++ js/slider.js
@@ -35,6 +35,100 @@ var changePos = slideWidth;
 displayTrans.innerText = slider.style.transform = "translateX(0px)";
 displayStartPos.innerText = startPos + "px";
 
+// Add event listeners for mouse and touch events
+slider.addEventListener('mousedown', handleMouseDown);
+slider.addEventListener('mouseup', handleMouseUp);
+slider.addEventListener('mousemove', handleMouseMove);
+slider.addEventListener('touchstart', handleTouchStart);
+slider.addEventListener('touchend', handleTouchEnd);
+slider.addEventListener('touchmove', handleTouchMove);
+
+// Add event listener for keyboard arrow keys
+document.addEventListener('keydown', handleKeyDown);
+
+// Function to handle mouse down event
+function handleMouseDown(event) {
+  event.preventDefault();
+  startPos = getTranslateXValue();
+  dragStartX = event.clientX;
+}
+
+// Function to handle mouse up event
+function handleMouseUp(event) {
+  event.preventDefault();
+  var dragEndX = event.clientX;
+  handleDrag(dragEndX);
+}
+
+// Function to handle mouse move event
+function handleMouseMove(event) {
+  event.preventDefault();
+  if (event.buttons === 1) {
+    var dragEndX = event.clientX;
+    handleDrag(dragEndX);
+  }
+}
+
+// Function to handle touch start event
+function handleTouchStart(event) {
+  event.preventDefault();
+  startPos = getTranslateXValue();
+  touchStartX = event.touches[0].clientX;
+}
+
+// Function to handle touch end event
+function handleTouchEnd(event) {
+  event.preventDefault();
+  var touchEndX = event.changedTouches[0].clientX;
+  handleDrag(touchEndX);
+}
+
+// Function to handle touch move event
+function handleTouchMove(event) {
+  event.preventDefault();
+  var touchEndX = event.touches[0].clientX;
+  handleDrag(touchEndX);
+}
+
+// Function to handle drag event
+function handleDrag(endX) {
+  var distance = endX - dragStartX;
+  var newPos = startPos + distance;
+
+  if (newPos > 0) {
+    // Dragged towards the right
+    newPos = 0;
+  } else if (newPos < -totalSlidesWidth) {
+    // Dragged towards the left
+    newPos = -totalSlidesWidth;
+  }
+
+  slider.style.transform = "translateX(" + newPos + "px)";
+  displayTrans.innerText = slider.style.transform;
+  displayStartPos.innerText = newPos + "px";
+}
+
+// Function to handle keyboard arrow keys
+function handleKeyDown(event) {
+  switch (event.keyCode) {
+    case 37: // left arrow key
+      prevBtn.click(); // Simulate a click on the previous button
+      break;
+    case 39: // right arrow key
+      nextBtn.click(); // Simulate a click on the next button
+      break;
+    default:
+      break;
+  }
+}
+
+// Utility function to get the current translateX value
+function getTranslateXValue() {
+  var transform = slider.style.transform;
+  var translateX = transform.match(/translateX\(([-\d]+)px\)/);
+  return translateX ? parseInt(translateX[1]) : 0;
+}
+
 prevBtn.addEventListener("click", function (e) {
   // If this is the first slide, show the last one in the slideshow instead
   if (startPos === 0) {
